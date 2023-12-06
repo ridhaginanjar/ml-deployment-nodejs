2b817ae2d9a8d3edd150fe5dd2807e52f9a68299
ridhaginanjar <ridhaginanjar7@gmail.com>
2023-12-06 18:03:13 +0700

    Update response API

--- src/handler.js
+++ src/handler.js
@@ -6,7 +6,7 @@ async function postPredictHandler(request, h) {
   // Make Predictions
   const { image } = request.payload;
   const { model } = request.server.app;
-  const { confidenceScore, label } = await predictClassification(model, image);
+  const { confidenceScore, label, explanation, suggestion } = await predictClassification(model, image);
 
   const id = crypto.randomUUID();
   const createdAt = new Date().toISOString();
@@ -14,6 +14,8 @@ async function postPredictHandler(request, h) {
   const data = {
     "id": id,
     "result": label,
+    "explanation": explanation,
+    "suggestion": suggestion,
     "confidenceScore": confidenceScore,
     "createdAt": createdAt
   }
--- src/service/inferenceService.js
+++ src/service/inferenceService.js
@@ -18,7 +18,28 @@ async function predictClassification(model, image) {
         const classResult = tf.argMax(prediction, 1).dataSync()[0];
         const label = classes[classResult];
 
-        return { confidenceScore, label };
+        let explanation, suggestion;
+
+        if(label === 'Melanocytic nevus') {
+            // Referensi: https://www.klikdokter.com/penyakit/masalah-kulit/nevus
+            explanation = "Melanocytic nevus adalah kondisi di mana permukaan kulit memiliki bercak warna yang berasal dari sel-sel melanosit, yakni pembentukan warna kulit dan rambut."
+            suggestion = "Segera konsultasi dengan dokter terdekat jika ukuran semakin membesar dengan cepat, mudah luka atau berdarah."
+        }
+
+        if(label === 'Squamous cell carcinoma') {
+            // Referensi: https://www.halodoc.com/kesehatan/karsinoma-sel-skuamosa
+            explanation = "Squamous cell carcinoma adalah jenis kanker kulit yang umum dijumpai. Penyakit ini sering tumbuh pada bagian-bagian tubuh yang sering terkena sinar UV."
+            suggestion = "Segera konsultasi dengan dokter terdekat untuk meminimalisasi penyebaran kanker."
+        }
+
+        if(label === 'Vascular lesion') {
+            // Referensi: https://www.ssmhealth.com/cardinal-glennon/services/pediatric-plastic-reconstructive-surgery/hemangiomas
+            explanation = "Vascular lesion adalah penyakit yang dikategorikan sebagai kanker atau tumor di mana penyakit ini sering muncul pada bagian kepala dan leher."
+            suggestion = "Segera konsultasi dengan dokter terdekat untuk mengetahui detail terkait tingkat bahaya penyakit."
+        
+        }
+
+        return { confidenceScore, label, explanation, suggestion };
     } catch (error) {
         throw new InputError(`Terjadi kesalahan input: ${error.message}`)
     }
